6de564a2eed2a6ada5af6e70d33187ba1ca82d52
Hunger <3287426755@qq.com>
2025-07-30 22:59:07 +0800

    refactor:Standardized format.

--- approval-server/src/main/java/fun/sast/service/UserService.java
+++ approval-server/src/main/java/fun/sast/service/UserService.java
@@ -1,13 +1,3 @@
 package fun.sast.service;
 
-import fun.sast.entity.User;
-
-public interface UserService {
-    /**
-     * 验证用户
-     *
-     * @param code 学号
-     * @param password 密码
-     */
-    User authenticate(String code, String password);
-}
+public interface UserService {}
--- approval-server/src/main/java/fun/sast/service/impl/UserServiceImpl.java
+++ approval-server/src/main/java/fun/sast/service/impl/UserServiceImpl.java
@@ -1,28 +1,7 @@
 package fun.sast.service.impl;
 
-import fun.sast.entity.User;
-import fun.sast.mapper.UserMapper;
 import fun.sast.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserServiceImpl implements UserService {
-
-    @Autowired private UserMapper userMapper;
-
-    /**
-     * 验证用户信息
-     *
-     * @param code 学号
-     * @param password 密码
-     * @return 用户信息
-     */
-    @Override
-    public User authenticate(String code, String password) {
-        //        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
-        //        queryWrapper.eq("code", code).eq("password", password);
-        //        return userMapper.selectOne(queryWrapper);
-        return new User();
-    }
-}
+public class UserServiceImpl implements UserService {}
