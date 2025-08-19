3ed6cf07c45fe11f7518b48847fa9e9a4a90a4f7
JosdaYK <15351929733@163.com>
2025-08-19 17:27:15 +0800

    Coding test fninshed

--- approval-server/src/main/java/fun/sast/ApprovalApplication.java
+++ approval-server/src/main/java/fun/sast/ApprovalApplication.java
@@ -4,10 +4,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @Slf4j
 @SpringBootApplication
 @MapperScan("fun.sast.mapper")
+@ComponentScan(basePackages = {"fun.sast"})
 public class ApprovalApplication {
     public static void main(String[] args) {
         SpringApplication.run(ApprovalApplication.class, args);
